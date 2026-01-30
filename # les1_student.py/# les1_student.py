a8adebb02ad6b5066dc14b9ad6a6779710d14d57
Shazaib6754 <9024659@student.zadkine.nl>
2026-01-30 09:54:52 +0100

    Add files via upload

---
+++ # les1_student.py/# les1_student.py
@@ -0,0 +1,17 @@
+# les1_student.py
+
+# Een class is een blauwdruk voor objecten.
+# Een object is een concrete instantie van een class met een eigen plek in het geheugen.
+
+class Student:
+    pass  # tijdelijke placeholder: de class is bewust leeg
+
+
+# Objecten (instances) maken van de class Student
+student1 = Student()
+student2 = Student()
+
+
+# Objecten printen
+print(student1)
+print(student2)
